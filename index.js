d73c1ce74c48d8974a6b80342a5c41894046a89c
DICKSON MUTWIRI GUANTAI <dicksonguantai@gmail.com>
2023-03-24 12:14:53 +0300

    Completed assignement

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById("input")
+input(addingEventListener());
+
 function addingEventListener() {
+     input.addEventListener('click', function (){
+        alert("I was clicked")
+    } );
+   
+
 }
